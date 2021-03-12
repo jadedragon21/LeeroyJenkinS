e8280c4ddda86b6eb1039753d7d920d1cc16fb16
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-03-12 10:09:25 -0600

    change 4.0

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.demo.controller.HomeController;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-class DemoApplicationTests {
+class MockwebApplicationTests {
+
+	@Autowired
+	private HomeController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
